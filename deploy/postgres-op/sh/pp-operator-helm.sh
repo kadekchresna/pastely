317bb0d25d89c224e0b9c3a371ebd305b9580afe
kadekchresna <kadekchresna@gmail.com>
2024-12-27 01:02:22 +0800

    adding deployment and migration

--- deploy/postgres-op/sh/pp-operator-helm.sh
+++ deploy/postgres-op/sh/pp-operator-helm.sh
@@ -3,4 +3,4 @@ helm repo add postgres-operator-charts https://opensource.zalando.com/postgres-o
 helm install postgres-operator postgres-operator-charts/postgres-operator
 
 helm repo add postgres-operator-ui-charts https://opensource.zalando.com/postgres-operator/charts/postgres-operator-ui && \
-helm install postgres-operator-ui postgres-operator-ui-charts/postgres-operator-ui
+helm install postgres-operator-ui postgres-operator-ui-charts/postgres-operator-ui -n default
